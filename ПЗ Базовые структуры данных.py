a4d31f0cb7f9735883b570ea4d2839d123aceca5
POPS1610 <krasnoyarsk.plop@gmail.com>
2024-10-28 20:24:31 +0700

    28/10

---
+++ ПЗ Базовые структуры данных.py
@@ -0,0 +1,16 @@
+#1st program
+print (9**0.5 * 5)
+
+#2st program
+print (9.99>9.98 and 1000!=1000.1)
+
+#3st program
+print (2*2+2)
+print (2*(2+2))
+print ((2*2+2)==(2*(2+2)))
+
+#4st program
+a = '123.456'
+b = float(a)*10
+c = int(b)%10
+print(c)
