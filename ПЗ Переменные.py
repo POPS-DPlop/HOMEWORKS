c824c8d87f7e7bf38cdcbb8cc695f57d3dfa8d61
POPS1610 <krasnoyarsk.plop@gmail.com>
2024-10-29 19:40:29 +0700

    28/10

---
+++ ПЗ Переменные.py
@@ -0,0 +1,5 @@
+homeworks = 12
+hours = 1.5
+name = 'Python'
+timeOfOne =  hours/homeworks
+print ('Курс:',name,', всего задач:',homeworks,', затрачено часов:',hours,', среднее время выполнения',timeOfOne,'часа.')
